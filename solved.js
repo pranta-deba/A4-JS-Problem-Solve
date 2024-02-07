789258cfa157a419f7d04463ab3ec314f35ede8a
Pranto Deb <rajprit0m@outlook.com>
2024-02-07 23:06:46 +0600

    problem 3 and 4 done

--- solved.js
+++ solved.js
@@ -12,13 +12,9 @@ function calculateMoney(ticket) {
   const profit = totalRs - (janitorSalary + lunchMoneyFor8People);
   return profit;
 }
-const result = calculateMoney(10);
-// console.log(result);
-
-
-
-
-
+const calculateMoneyResult = calculateMoney(10);
+// console.log(calculateMoneyResult);
+// --------------------------------------------------------------------------------
 function checkName(name) {
   if (typeof name !== "string") {
     return "invalid!";
@@ -41,6 +37,60 @@ function checkName(name) {
     return "Bad Name";
   }
 }
+const checkNameResult = checkName("RAFEE");
+// console.log(checkNameResult);
 
-const result2 = checkName("RAFEE");
-console.log(result2);
+// --------------------------------------------------------------------------------
+
+function deleteInvalids(array) {
+  if (!Array.isArray(array)) {
+    return "Sorry, the initial array must be given!";
+  } else if (array.length === 0) {
+    return "An empty array cannot be given!";
+  }
+
+  let onlyNumArray = [];
+
+  for (const element of array) {
+    if (typeof element === "number" && !isNaN(element)) {
+      onlyNumArray.push(element);
+    }
+  }
+  return onlyNumArray;
+}
+const deleteInvalidsResult = deleteInvalids([
+  1,
+  null,
+  undefined,
+  18,
+  -19,
+  NaN,
+  "12",
+  [1, 2],
+  { ob: "lala" },
+]);
+// console.log(deleteInvalidsResult);
+
+// --------------------------------------------------------------------------------
+
+function password(obj) {
+  if (typeof obj !== "object" || Array.isArray(obj) || obj === null) {
+    return "invalid";
+  } else if (
+    obj.name === undefined ||
+    obj.birthYear === undefined ||
+    obj.siteName === undefined
+  ) {
+    return "invalid";
+  } else if (obj.birthYear.toString().length !== 4) {
+    return "invalid";
+  }
+
+  const websiteName = obj.siteName[0].toUpperCase() + obj.siteName.slice(1);
+  const strongPass = websiteName + "#" + obj.name + "@" + obj.birthYear;
+  return strongPass;
+}
+const passwordResult = password({name: "kolimuddin",birthYear: 1958,siteName: "google"});
+// console.log(passwordResult);
+
+// --------------------------------------------------------------------------------
